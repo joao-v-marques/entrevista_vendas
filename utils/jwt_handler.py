bca76ffcf4e8ffe129568ff0325939637d3e246f
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-14 15:14:37 -0300

    fix: ajustado informações salvas no payload, tinha informações de outro projeto

--- utils/jwt_handler.py
+++ utils/jwt_handler.py
@@ -5,9 +5,8 @@ def generated_token(user):
     payload = {
         "id": user.id,
         "username": user.username,
-        "role_description": user.role,
-        "maintenance_group_id": user.maintenance_group_id,
-        "maintenance_group": user.maintenance_group,
+        "role_name": user.role_name,
+        "sector_name": user.sector_name,
         "exp": datetime.utcnow() + timedelta(hours=8)
     }
 
