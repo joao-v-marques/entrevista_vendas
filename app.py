132fc87107f19aea9b06bcfe360b1c9ee5fd2c64
joao-v-marques <joaovmarques2006@outlook.com>
2026-08-04 14:36:58 -0300

    fix: ajustado porta da aplicação para rodar na 5005

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
         app.run(debug=True)
     else:
         print("Servidor Waitress iniciado com sucesso...")
-        serve(app, host='0.0.0.0', port=5000, threads=8)
+        serve(app, host='0.0.0.0', port=5005, threads=8)
