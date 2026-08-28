a61e36a1487bfdf3330f146758a38704235a60f6
joao-v-marques <joaovmarques2006@outlook.com>
2026-08-28 14:04:35 -0300

    feat: adicionado função de get_by_id

--- models/sectors_models.py
+++ models/sectors_models.py
@@ -40,3 +40,35 @@ class SectorModel:
                 cursor.close()
             if conn:
                 conn.close()
+
+    # GET pelo id, usado para validar o setor informado no cadastro/edição de usuários antes
+    # de o INSERT esbarrar na foreign key
+    @staticmethod
+    def get_by_id(sector_id):
+        conn = None
+        cursor = None
+        try:
+            conn, cursor = get_db_connection()
+
+            sql_query = """
+                SELECT *
+                FROM sectors
+                WHERE id = %s
+            """
+            values = (sector_id,)
+
+            cursor.execute(sql_query, values)
+
+            sector_data = cursor.fetchone()
+
+            if not sector_data:
+                return None
+
+            return Sector(**sector_data)
+        except Exception as e:
+            raise Exception(str(e))
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
